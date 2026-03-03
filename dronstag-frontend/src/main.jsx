e96969f49c74162f5767f1f4810f01ddc4a5aee5
AMXB <szalaiben2411@gmail.com>
2026-03-03 12:19:11 +0100

    Aha

--- dronstag-frontend/src/main.jsx
+++ dronstag-frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+// Alapértelmezetten VILÁGOS módban indulunk
+// Töröljük a localStorage-ból a sötét mód beállítást ha volt
+localStorage.removeItem('theme');
+// Eltávolítjuk a dark class-t a html elemről
+document.documentElement.classList.remove('dark');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
